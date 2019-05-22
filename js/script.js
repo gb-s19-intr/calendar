8c9be14fb17787d540c8b8f02838fc32b35d55c8
Graphic <graphic@WFD201-00M.local>
2019-05-22 10:06:09 -0400

    Cleanup HTML/CSS and fixed convertMinutesToTime

--- js/script.js
+++ js/script.js
@@ -15,14 +15,14 @@ const appointment = {
   priority: `critical`,
 };
 
-const convertMinutesToTime = (minOfDay) => {
-  const hr = minOfDay / 60;
-  const min = minOfDay - hr * 60;
-  return `${hr}:${min}`;
+// Take a number of minutes in a day (0 to 1440, note it doesn't check though!) and converts it to a readable military time
+const convertMinutesToTime = (minOfDay = 0) => {
+  const hr = Math.floor(minOfDay / 60);   // Round the time down to an integer
+  const min = Math.floor(minOfDay - hr * 60); // Round the minutes down to an integer
+  return `${ (hr < 10)?`0`+hr:hr }:${ (min < 10)?`0`+min:min} `; // return a string, format with '0's
 }
 
 // Write a function called formatAppointment that takes an appointment object as an argument and returns a String of formatted HTML. The return String should have all the relevant properties from the appointment Object embedded within it. Meaning any appointment passed into the function would result in a unique String of HTML being return that's specific to that appointment.
-
 const formatAppointment = (appt) => {
 
   return `<li class="appt cat-${appt.category}">
