ab54164e5e96dd5a2a16fceee1c1cc7a4e6148bb
Graphic <graphic@WFD201-00M.local>
2019-05-22 14:57:13 -0400

    Data set using arrays

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-const appointment1 = {
+// ******************************************************
+// *********************** DATA *************************
+// ******************************************************
+
+// One appointment Object
+const appointment0 = {
   name: `Work on appointment structure`,
   location: `2 Lower Jarvis St.`,
   date: {
@@ -11,11 +16,12 @@ const appointment1 = {
     duration: 60,
   },
   notes: `This is awesome!`,
-  category: `school`,
+  category: [`school`],
   priority: `low`,
 };
 
-const appointment2 = {
+// Another appointment Object
+const appointment1 = {
   name: `Insert appointments into the interface`,
   location: `2 Lower Jarvis St.`,
   date: {
@@ -28,12 +34,24 @@ const appointment2 = {
     duration: 240,
   },
   notes: `This is awesome!`,
-  category: `work`,
+  category: [`work`, `school`],
   priority: `critical`,
 };
 
+// Add both Objects to an Array, making this a table!
+const allAppointments = [
+  appointment0,
+  appointment1,
+];
+
+console.log(allAppointments);   // Dump out the raw data to the console
+console.table(allAppointments); // Format the data to look like a table (in the console)
 
 
+
+// ********************************************************
+// ****************** HELPER FUNCTIONS ********************
+// ********************************************************
 
 // Take a number of minutes in a day (0 to 1440, note it doesn't check though!) and converts it to a readable military time
 const convertMinutesToTime = (minOfDay = 0) => {
@@ -45,18 +63,30 @@ const convertMinutesToTime = (minOfDay = 0) => {
 // Write a function called formatAppointment that takes an appointment object as an argument and returns a String of formatted HTML. The return String should have all the relevant properties from the appointment Object embedded within it. Meaning any appointment passed into the function would result in a unique String of HTML being return that's specific to that appointment.
 const formatAppointment = (appt) => {
   // Returns a string of HTML ready to insert
-  return `<li class="appt cat-${appt.category} ${appt.priority}" style="height: ${appt.time.duration}px;">
+  return `<li class="appt cat-${appt.category[0]} ${appt.priority}" style="height: ${appt.time.duration}px;">
             <div class="start-time">${convertMinutesToTime(appt.time.start)}</div>
             <div>${appt.name}</div>
           </li>`;
 }
 
+
+
+// ********************************************************
+// ******************* DOM REFERENCES *********************
+// ********************************************************
+
 // Step 1: Store a reference to the object we want to modify
-const allAppts = document.getElementById(`todays-appts`);  // <ul>
+const allAppts = document.getElementById(`todays-appts`);     // <ul>
 
 // You can also select the first matching element by CSS selector like this:
 // const allAppts = document.querySelector(`.appointments`);  // <ul>
 
-allAppts.innerHTML += formatAppointment(appointment1);
-allAppts.innerHTML += formatAppointment(appointment2);
 
+
+// ********************************************************
+// ******************* APPLIATION CODE ********************
+// ********************************************************
+
+allAppts.innerHTML += formatAppointment(appointment0);
+allAppts.innerHTML += formatAppointment(appointment1);
+allAppts.innerHTML += `Hello world!!`;
