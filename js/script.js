48f06035429111eb9035306cbfba5456d53baddc
Graphic <graphic@WFD201-00M.local>
2019-05-22 11:44:00 -0400

    Updated document output

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const appointment = {
+const appointment1 = {
   name: `Work on appointment structure`,
   location: `2 Lower Jarvis St.`,
   date: {
@@ -7,13 +7,33 @@ const appointment = {
     day: 15,
   },
   time: {
-    start: 0,
-    end: 1440,
+    start: 400,
+    duration: 60,
   },
   notes: `This is awesome!`,
   category: `school`,
+  priority: `low`,
+};
+
+const appointment2 = {
+  name: `Insert appointments into the interface`,
+  location: `2 Lower Jarvis St.`,
+  date: {
+    year: 2019,
+    month: 5,
+    day: 22,
+  },
+  time: {
+    start: 540,
+    duration: 240,
+  },
+  notes: `This is awesome!`,
+  category: `work`,
   priority: `critical`,
 };
+
+
+
 
 // Take a number of minutes in a day (0 to 1440, note it doesn't check though!) and converts it to a readable military time
 const convertMinutesToTime = (minOfDay = 0) => {
@@ -24,11 +44,19 @@ const convertMinutesToTime = (minOfDay = 0) => {
 
 // Write a function called formatAppointment that takes an appointment object as an argument and returns a String of formatted HTML. The return String should have all the relevant properties from the appointment Object embedded within it. Meaning any appointment passed into the function would result in a unique String of HTML being return that's specific to that appointment.
 const formatAppointment = (appt) => {
-
-  return `<li class="appt cat-${appt.category}">
-            ${convertMinutesToTime(appt.time.start)}
-            ${appt.name}
+  // Returns a string of HTML ready to insert
+  return `<li class="appt cat-${appt.category} ${appt.priority}" style="height: ${appt.time.duration}px;">
+            <div class="start-time">${convertMinutesToTime(appt.time.start)}</div>
+            <div>${appt.name}</div>
           </li>`;
 }
 
-console.log( formatAppointment(appointment) );
+// Step 1: Store a reference to the object we want to modify
+const allAppts = document.getElementById(`todays-appts`);  // <ul>
+
+// You can also select the first matching element by CSS selector like this:
+// const allAppts = document.querySelector(`.appointments`);  // <ul>
+
+allAppts.innerHTML += formatAppointment(appointment1);
+allAppts.innerHTML += formatAppointment(appointment2);
+
